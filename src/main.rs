8622ea54e639e68649993abe9f1017ace171a267
Derek <dehrexd@gmail.com>
2025-06-26 04:56:33 +0000

    basic main menu list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use cursive::{Cursive, CursiveExt};
+use cursive::views::{Dialog, SelectView};
+use cursive::align::{HAlign};
+
 fn main() {
-    println!("Hello, world!");
+	let mut root = Cursive::new();
+    root.add_global_callback('q', |s| s.quit());
+    
+    let main_view = Dialog::around(
+        
+        SelectView::new().h_align(HAlign::Center)
+        .item("Source", 1)
+        .item("Hostname", 2)
+        .item("Locale", 3)
+        .item("Keyboard", 4)
+        .item("Timezone", 5)
+        .item("Root Password", 6)
+        .item("Additional Users", 7)
+        .item("Partitioning", 8)
+        .item("Setup Bootloader", 9)
+        .item("Additional Repositories", 10)
+        .item("Install", 11))
+        
+        .title("Chimera Linux Installer");
+
+    root.add_layer(main_view);
+
+    root.run()
 }
